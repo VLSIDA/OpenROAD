c18b023d45d2729474ff99052734069a4b4c3f64
gmejiamtz <gmejiama@ucsc.edu>
2025-05-13 01:58:25 -0700

    Included the correct cms/ClockMesh.hh in ClockMesh.cc, corrected include dir in cms/CMakeLists.txt

--- src/cms/src/ClockMesh.cc
+++ src/cms/src/ClockMesh.cc
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include "cms/Tool.hh"
+#include "cms/ClockMesh.hh"
 #include "sta/StaMain.hh"
 
 namespace sta {
