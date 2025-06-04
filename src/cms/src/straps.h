5ac644ce227f0e83c7d36df68faac80e89038b0c
Omkar Lonkar <olonkar@keylime.soe.ucsc.edu>
2025-06-04 13:20:08 -0700

    Slight typo in argument declaration

--- src/cms/src/straps.h
+++ src/cms/src/straps.h
@@ -55,7 +55,7 @@ class Straps
                   int abs_start,
                   int abs_end,
                   bool is_delta_x,
-                  const Shape::ObstructionTree& avoid);
+                  const ObstructionTree& avoid);
 
   int getNumberOfStraps();
 
